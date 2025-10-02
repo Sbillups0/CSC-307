3e73d657d30e019db5ec8c670e8d539fafa8a2bf
Sbillups0 <sbillups0@me.com>
2025-10-02 12:00:12 -0700

    thing

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,7 +3,7 @@ import express from "express";
 
 const app = express();
 const port = 8000;
-
+app.use(express.json());
 const users = {
     users_list: [
       {
@@ -40,18 +40,63 @@ const findUserByName = (name) => {
   );
 };
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const findUserByJob = (job) =>
+    users["users_list"].filter((user) => user["job"] === job);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.get("/users/:name/:job", (req, res) => {
+  const name = req.params["name"]; //or req.params.id  
+  const job = req.params["job"];
   if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
+    let resultByJob = findUserByJob(job);
+    resultByJob = { users_list: resultByJob };
+    if (result["users_list"].length === 0 || resultByJob["users_list"].length === 0) {
+        res.status(404).send("Resource not found.");
+        return;
+    }
     res.send(result);
   } else {
     res.send(users);
   }
 });
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    }
+    else {
+        users["users_list"] = users["users_list"].filter((user) => user["id"] !== id);
+        res.send(`User with id ${id} deleted.`);
+    }
+})
 
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
